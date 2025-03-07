95158e6147a99b101e82b809165af1ca240c4be8
Alfred-Arinze <fredintek@gmail.com>
2025-03-07 09:08:48 +0200

    dockerized backend

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export default registerAs('database', () => ({
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
-  databaseName: process.env.DATABASE_NAME,
+  databaseName: process.env.MYSQL_DATABASE,
   synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
   autoloadEntities:
     process.env.DATABASE_AUTOLOAD_ENTITIES === 'true' ? true : false,
